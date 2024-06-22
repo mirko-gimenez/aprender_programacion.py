fe4673df09cf3f69cf72a78373245f395b145ab1
soy mirko <mirkoagimenez@gmail.com>
2024-06-22 18:38:47 -0300

    agregando el primer arc.

---
+++ logica_brais/logica_de_prog._.py
@@ -0,0 +1,3 @@
+#la logica de programacion es el proceso de aplicar una secuencia estructura y coherente de pasos para resolver problemas especificos mediante un lenguaje.
+# pagina retos de programacion. lo importante no es el lenguaje sino nuestra capacidad de entender los fundamentos y rosolvel problemas.
+#descargar twich.        . 
