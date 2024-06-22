2a9786a0fa799d741d7880a1487214b5ebabfbe1
soy mirko <mirkoagimenez@gmail.com>
2024-06-22 18:40:14 -0300

    archivo de prubea

---
+++ curso_py_dalto/operadores _aritmeticos.py
@@ -0,0 +1,33 @@
+#suma y resta (+ y -)
+suma = 1 + 2
+
+resta = 6 -3
+
+#multiplicacion y divicion (* y /)
+multi = 3 * 1
+
+division = 12 / 5 # devuelve un float date
+
+#potenciacion (exponente) (**)
+
+exponente = 12 ** 5
+
+#division_baja (//)
+
+baja = 12 // 5 # devuelve int. redondeado hacia abajo.
+
+#modulo o resto (%)
+
+resto = 12 % 5
+
+print(suma, resta, multi, division, exponente, baja, resto)
+
+tipo_de_dato = type(5) # (type) muestra el tipo de dato
+
+tipo_de_dato_2 = type("holamundo")
+
+print(tipo_de_dato_2)
+
+print(tipo_de_dato)
+
+#finaliza en 1:19:44
