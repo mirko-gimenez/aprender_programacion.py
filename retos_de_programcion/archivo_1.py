853c43d83884d278bd77f29b520ec3ede16707a0
Mirko <mirkoagimenez@gmail.com>
2024-06-24 11:36:23 -0300

    primer ejercicio

---
+++ retos_de_programcion/archivo_1.py
@@ -0,0 +1,17 @@
+#https://www.python.org/
+"""se utiliza triples comillas, para:"""
+'''comentar texto en varias lineas ya sean "dobles" o 'simples' '''
+
+#las variables  CONSTANTES en python no se soportan, se representan en MAYUSCULAS.
+
+tipoint = 45
+
+tipofloat = 1.3
+
+tipobool = True
+
+tipostring = "somos campeones del mundo"
+
+tiponome = None
+
+print("¡hola, Python!")
